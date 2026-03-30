57d935eedcb2f8967b8a67ca03b1da920b62bea6
Luis Cipriani <cipriani.luisguilherme@gmail.com>
2026-03-30 16:30:13 -0300

    Cria classe Order

---
+++ PedidosCliente/Entities/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PedidosCliente.Entities;
+
+namespace PedidosCliente.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order()
+        {}
+
+        public Order(DateTime Moment, OrderStatus status, Client client)
+        {
+            Moment = Moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+
+        }
+    }
+}
---
+++ PedidosCliente/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PedidosCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
