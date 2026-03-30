8a33d4259ae69ff09e655f8c65ea859b2459dd93
Luis Cipriani <cipriani.luisguilherme@gmail.com>
2026-03-30 17:44:19 -0300

    Implementa ToString da classe Order

--- PedidosCliente/Entities/Order.cs
+++ PedidosCliente/Entities/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 using PedidosCliente.Entities;
 using PedidosCliente.Entities.Enums;
 
@@ -41,6 +42,22 @@ namespace PedidosCliente.Entities
                 total += item.SubTotal();
             }
             return total;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);
+            sb.AppendLine("Order items:");
+            foreach(OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
         }
     }
 }
